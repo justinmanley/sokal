553f1d4cc10448016013ead5036e86d8f5054431
Justin Manley <manleyjster@gmail.com>
2015-04-27 01:12:24 -0500

    Fix HTTP errors caused by 302 responses. Stub out HTML parsing.

--- Suck.hs
+++ Suck.hs
@@ -1,10 +1,11 @@
-module Suck where
+module Suck (primitiveModel, suck) where
 
 import Control.Applicative ((<$>))
 import Text.XML.HXT.Core
 import Text.XML.HXT.HTTP
 import Data.Map (Map, (!))
 import qualified Data.Map as Map
+import Text.HandsomeSoup (css)
 
 type PrimitiveModel = Map (String, String) [String]
 type ProcessedModel = [(String, [(Int, Int)])]
@@ -13,8 +14,12 @@ type ProcessedModel = [(String, [(Int, Int)])]
 {-mkPrimitiveModel pages = do-}
     {-map (readString [withParseHTML yes]) pages-}
 
+primitiveModel page = page 
+    >>> css "#body"
+    //> getText
+
 suck :: IO ()
 suck = do
-    results <- mapM (runX . readDocument [withParseHTML yes, withHTTP []]) =<< 
+    results <- mapM (runX . primitiveModel . readDocument [withParseHTML yes, withHTTP [], withWarnings no, withRedirect yes]) =<< 
         lines <$> readFile "urls.txt"
     putStrLn "hello"
