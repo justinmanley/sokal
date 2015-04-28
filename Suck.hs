e98b852b6bd35273f559ed8fd2537ecc3eeb4fa3
Justin Manley <manleyjster@gmail.com>
2015-04-27 19:16:11 -0500

    Process HTML documents into a primitive model.

--- Suck.hs
+++ Suck.hs
@@ -1,25 +1,39 @@
-module Suck (primitiveModel, suck) where
+module Suck (getArticleBody, suck, hxtConfig) where
 
 import Control.Applicative ((<$>))
 import Text.XML.HXT.Core
 import Text.XML.HXT.HTTP
 import Data.Map (Map, (!))
-import qualified Data.Map as Map
+import qualified Data.Map as Map hiding (foldr)
 import Text.HandsomeSoup (css)
 
 type PrimitiveModel = Map (String, String) [String]
 type ProcessedModel = [(String, [(Int, Int)])]
 
-{-mkPrimitiveModel :: [String] -> PrimitiveModel-}
-{-mkPrimitiveModel pages = do-}
-    {-map (readString [withParseHTML yes]) pages-}
+hxtConfig :: [SysConfig]
+hxtConfig = 
+    [ withParseHTML yes
+    , withHTTP []
+    , withWarnings no
+    , withRedirect yes ]
 
-primitiveModel page = page 
+getArticleBody page = page 
     >>> css "#body"
     //> getText
 
+primitiveModel :: PrimitiveModel -> [String] -> PrimitiveModel
+primitiveModel model [] = model
+primitiveModel model [x] = model
+primitiveModel model [x,y] = model
+primitiveModel model (x1:x2:x3:xs) = primitiveModel newModel (x2:x3:xs) where
+    newModel = Map.insertWith (++) (x1, x2) [x3] model    
+
+--processModel :: PrimitiveModel -> ProcessedModel
+--processModel prim =  
+
 suck :: IO ()
 suck = do
-    results <- mapM (runX . primitiveModel . readDocument [withParseHTML yes, withHTTP [], withWarnings no, withRedirect yes]) =<< 
-        lines <$> readFile "urls.txt"
+    results <- mapM ((fmap $ concatMap words) . runX . getArticleBody . readDocument hxtConfig) 
+        =<< lines <$> readFile "urls.txt"
+    let modelPrim = foldr (Map.unionWith (++)) Map.empty (map (primitiveModel Map.empty) results)
     putStrLn "hello"
