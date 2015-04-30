61a3d67512d15b4dca7725be02851f95221b7a42
Justin Manley <manleyjster@gmail.com>
2015-04-29 19:40:26 -0500

    Finish genText and spew using WriterT paramaterized over (State StdGen).

--- Spew.hs
+++ Spew.hs
@@ -4,6 +4,7 @@ import Data.Array
 import System.Random
 import Control.Monad.Trans.State.Lazy
 import Control.Monad.Writer.Lazy
+import Control.Applicative ((<$>))
 
 type Id = Int
 type Rand = State StdGen
@@ -15,28 +16,33 @@ parseModel str = array size $ zipWith parseState [1..] states where
     size = (1, length states)
     parseState i str = (i, read str)
 
-select :: [(Int, b)] -> Rand b
-select xs = 
+select :: [a] -> Rand a
+select xs = fmap (xs !!) . state . randomR $ (0, length xs - 1)
+
+frequency :: [(Int, b)] -> Rand b
+frequency xs = 
     let frequencies = fst . unzip $ xs
-        size = (1, sum frequencies)
         ids = concatMap (\(freq, id) -> replicate freq id) xs
     in do
-        rand <- state $ randomR size
-        return $ (listArray size ids) ! rand
-
-
+        rand <- state $ randomR (0, sum frequencies - 1)
+        return $ ids !! rand
 
 genText :: Id -> FastModel -> WriterT String Rand Int 
 genText start model = iter start where
-    iter state = undefined --do
---        let (word, successors) = model ! word
---        succ <- select successors
-        
---        tell word
+    iter state = do
+        let (word, successors) = model ! state
+        tell word -- emit a word
+    
+        case successors of
+            [] -> iter =<< lift (select $ indices model)
+            _  -> iter =<< lift (frequency successors)
 
-        
-
-             
 spew :: IO ()
 spew = do
+    model <- parseModel <$> readFile "sokal.model"
+    gen <- getStdGen
+    let start = fst $ randomR (bounds model) gen 
+
     putStrLn "hello"
+    writeFile "sokal.output" 
+        $ (flip evalState $ gen) . execWriterT . genText start $ model  
